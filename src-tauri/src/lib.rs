1ab9979236f8e09de969060a64fe2605d69867c4
Entlst <1515240396@qq.com>
2026-01-31 21:58:21 +0800

    modify icon&name

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,7 +6,7 @@ use uniaz::UniAz;
 fn is_chinese(c: char) -> bool {
     matches!(c,
         '\u{4E00}'..='\u{9FFF}' |  // CJK 统一汉字
-        '\u{3400}'..='\u{4DBF}' |  // CJK 扩展 A
+        '\u{3400}'..='\u{4DBF}' |  // CJK 扩展 A    TODO: 扩A一部分文字加密后为3个字母，暂未作处理
         '\u{F900}'..='\u{FAFF}'    // CJK 兼容汉字
     )
 }
