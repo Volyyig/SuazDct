a1bb237516088ad1d3e851796d92d8babb1867e8
Entlst <1515240396@qq.com>
2026-03-01 22:19:04 +0800

    fix(ui): 安卓端调起/收起输入法时，输入法窗口变化引起的界面震颤

--- src/App.vue
+++ src/App.vue
@@ -51,9 +51,12 @@ onMounted(() => {
 .app {
   display: flex;
   flex-direction: column;
-  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   margin: 0 auto;
-  position: relative;
 }
 
 .content {
